a98bfcea5ae32412b130092c2ea18f84d52286d0
zengyazy <49637848+zengyazy@users.noreply.github.com>
2022-01-16 20:53:02 +0800

    Update utils_Ent_camrest.py

--- utils/utils_Ent_camrest.py
+++ utils/utils_Ent_camrest.py
@@ -180,8 +180,8 @@ def generate_memory(sent, speaker, time):
 
 def prepare_data_seq(batch_size=100):
     file_train = 'data/CamRest/CamRest676_train.txt'
-    file_dev = 'data/CamRest/{}CamRest676_dev.txt'
-    file_test = 'data/CamRest/{}CamRest676_test.txt'
+    file_dev = 'data/CamRest/CamRest676_dev.txt'
+    file_test = 'data/CamRest/CamRest676_test.txt'
 
     pair_train, train_max_len = read_langs(file_train, max_line=None)
     pair_dev, dev_max_len = read_langs(file_dev, max_line=None)
